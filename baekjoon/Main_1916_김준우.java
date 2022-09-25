b07a22e3dbca2773a76bb00956f1b224099a8bd2
junwoo <junwu19@naver.com>
2022-09-26 00:00:29 +0900

    [ADD]adding solved problem

---
+++ baekjoon/Main_1916_김준우.java
@@ -0,0 +1,8 @@
+
+public class Main_1916_김준우 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringToke
+        System.out.println();
+    }
+}
