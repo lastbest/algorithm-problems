61dcd43fbc45256b0776d79a329a405b50799109
junwoo <junwu19@naver.com>
2022-10-17 20:17:41 +0900

    [ADD]adding solved problem

---
+++ baekjoon/DynamicProgramming/Main_16123_김준우.java
@@ -0,0 +1,10 @@
+package _0401_dynamicprogramming;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_16123_김준우 {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
